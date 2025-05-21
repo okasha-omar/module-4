8da4db9279554103cb262cb4f1343af8a8bfca47
okasha-omar <mokashaomar@gmail.com>
2025-05-21 13:54:37 -0500

    Update pirate fixed.py

--- pirate fixed.py
+++ pirate fixed.py
@@ -1,5 +1,5 @@
 """
-Name: Mitansh
+Name: Okasha Omar Mohammed
 Date: 05/03/2024
 Description: This simple program checks if the user knows the pirate password ("Arrr!").
              If they do, it identifies them as a pirate; otherwise, it greets them
